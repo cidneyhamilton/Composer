addfc6492f520f55e38eea07973ddfb791f0eb68
Cidney Hamilton <cidney.hamilton@gmail.com>
2019-03-17 16:34:32 -0400

    Remove the Props tag from the scene view for Summer Daze.

--- App/features/scenes/sceneEditor.js
+++ App/features/scenes/sceneEditor.js
@@ -3,11 +3,16 @@
         SceneDetail = require('./sceneDetail'),
         PropsIndex = require('features/props/index');
 
+    var selectedGame = require('features/projectSelector/index');
+
     var ctor = function(owner, entry) {
         Editor.call(this, owner, entry, new SceneDetail(), 'sceneId');
         Editor.currentSceneId = entry.id;
         
-        this.screens.splice(1, 0, new PropsIndex(this, entry.id));
+        // Show props for this scene if there are props in the UI
+        if (selectedGame.showAdvanced) {
+            this.screens.splice(1, 0, new PropsIndex(this, entry.id));
+        }
     };
 
     Editor.baseOn(ctor);

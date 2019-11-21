13a0dfc7a5956c84643e110d9762ada966c683f0
Cidney Hamilton <cidney@cidneyhamilton.com>
2019-11-21 15:49:14 -0500

    Add a Moxie stat to the character sheet.

--- App/features/components/simpleCharSheet.js
+++ App/features/components/simpleCharSheet.js
@@ -7,6 +7,7 @@ define(function (require) {
 		// TODO: Define this list from Composer
 		this.smarts = attributes.smarts || 0;
 		this.fitness = attributes.fitness || 0;
+		this.moxie = attributes.moxie || 0;
         this.charm = attributes.charm || 0;
 		this.skills = attributes.skills || 0;
         this.luck = attributes.luck || 0;

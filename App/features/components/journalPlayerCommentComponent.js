b57b910bfd43a383065399eef39e428ff52d8d13
Cidney Hamilton <cidney@cidneyhamilton.com>
2020-10-13 15:45:43 -0400

    Add translation notes for Summer Daze-specific journal text components.

--- App/features/components/journalPlayerCommentComponent.js
+++ App/features/components/journalPlayerCommentComponent.js
@@ -1,20 +1,33 @@
 define(function(require) {
-	var ctor = function(attributes) {
-		attributes = attributes || {};
-		attributes.comments = attributes.comments || [];
-		
-		this.type = ctor.type;
-		this.comments = attributes.comments;		
-	};
-
-	ctor.displayName = "Player's Comments about Character in Journal";
-	ctor.type = "components.journalPlayerCommentComponent";
-
-	var compatibleTypes = ['actor'];
-	ctor.isCompatibleWith = function(entity) {
-		return compatibleTypes.indexOf(entity.type) != 1;
-	};
-
-	return ctor;
+    var ctor = function(attributes) {
+	attributes = attributes || {};
+	attributes.comments = attributes.comments || [];
 	
+	this.type = ctor.type;
+	this.comments = attributes.comments;
+	this.notes = attributes.notes || [];
+    };
+    
+    ctor.displayName = "Player's Comments about Character in Journal";
+    ctor.type = "components.journalPlayerCommentComponent";
+    
+    var compatibleTypes = ['actor'];
+    ctor.isCompatibleWith = function(entity) {
+	return compatibleTypes.indexOf(entity.type) != 1;
+    };
+
+    ctor.prototype.localize = function(localizationId, context) {
+	var note, comment;
+	for (var i = 0; i < comments.length; i++) {
+	    comment = comments[i];
+	    if (i < notes.length) {
+		note = notes[i];
+	    }
+	    context.addLocalizationEntry(comment, note);
+	}
+
+    };
+    
+    return ctor;
+    
 });
--- App/features/components/journalQuoteComponent.js
+++ App/features/components/journalQuoteComponent.js
@@ -1,19 +1,25 @@
 define(function(require) {
-	var ctor = function(attributes) {
-		attributes = attributes || {};
-
-		this.type = ctor.type;
-		this.quote = attributes.quote || "";
-	};
-
-	ctor.displayName = "Journal Quote";
-	ctor.type = "components.journalQuoteComponent";
-
-	var compatibleTypes = ['actor'];
-	ctor.isCompatibleWith = function(entity) {
-		return compatibleTypes.indexOf(entity.type) != 1;
-	};
-
-	return ctor;
+    var ctor = function(attributes) {
+	attributes = attributes || {};
 	
+	this.type = ctor.type;
+	this.quote = attributes.quote || "";
+	this.notes = attributes.notes || "";
+    };
+    
+    ctor.displayName = "Journal Quote";
+    ctor.type = "components.journalQuoteComponent";
+    
+    var compatibleTypes = ['actor'];
+    ctor.isCompatibleWith = function(entity) {
+	return compatibleTypes.indexOf(entity.type) != 1;
+    };
+
+    // Allow the journal to be translated.
+    ctor.prototype.localize = function(context) {
+	context.addLocalizationEntry(this.quote, this.quote);
+    };
+    
+    return ctor;
+    
 });
